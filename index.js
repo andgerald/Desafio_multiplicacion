c626d9b7974731b33fe815da0fd67fce015098a0
andgeral <geraldinebecerra93@gmail.com>
2024-02-15 23:24:29 -0300

    se agrega funcion que calcule la multiplicacion y factorial de x numero

---
+++ index.js
@@ -0,0 +1,14 @@
+const multiplicacion = (num) => {
+  if (num > 0 && num <= 20) {
+    let factorial = 1;
+    for (let i = 1; i <= num; i++) {
+      let multi = num * i;
+      for (let j = 1; j > 0; j--) {
+        factorial = factorial * i;
+      }
+      console.log(i, "x", num, "=", multi);
+      console.log("el factorial de ", i, "es:", factorial);
+    }
+  }
+};
+multiplicacion(3);
